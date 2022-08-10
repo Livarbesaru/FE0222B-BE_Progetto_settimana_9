aef5ffa959f84b9c3827b70b6cd9e7c7f4afaf35
Livarbesaru <liburdidaniele1998@gmail.com>
2022-08-10 13:00:49 +0200

    commit2

--- src/main/java/com/film/ws/FilmRestController.java
+++ src/main/java/com/film/ws/FilmRestController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/film")
-@Api(value = "FilmRest", tags = "Gestione film")
 public class FilmRestController {
 
 	
